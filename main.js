c68dc9a1602148e4365c7081f970fcbddb9231e5
MorZluf <morzluf19@gmail.com>
2019-03-26 14:39:53 +0200

    adding office-challenge

--- main.js
+++ main.js
@@ -1,2 +1,65 @@
-//write your code here to make the tests pass
+
+class Document {
+    constructor(EmployeeName) {
+        this.EmployeeName = EmployeeName
+    }
+}
+
+class Employee {
+    constructor(name) {
+        this.name = name
+    }
+    work(office) {
+        for (let i = 0; i < 10; i++) {
+            document[i] = new Document(this.name)
+            office.documents.push(document[i])
+        }
+    }
+}
+
+class Manager {
+    constructor(name, employees) {
+        this.name = name
+        this.employees = []
+    }
+
+    hireEmployee(name) {
+        const newEmployee = new Employee(name)
+        this.employees.push(newEmployee)
+    }
+
+    askEmployeesToWork(office) {
+        this.employees.forEach(e => e.work(office))
+    }
+}
+
+
+class Cleaner {
+    constructor(name) {
+        this.name = name
+    }
+    clean() {
+        console.log("Clean")
+    }
+}
+
+class Office {
+    constructor() {
+        this.documents = []
+        this.managers = []
+        this.cleaners = []
+    }
+    hireCleaner(name) {
+        const newCleaner = new Cleaner(name)
+        this.cleaners.push(newCleaner)
+    }
+    hireManager(name) {
+        const newManager = new Manager(name)
+        this.managers.push(newManager)
+    }
+    startWorkDay() {
+        this.managers.forEach(m => m.askEmployeesToWork(this))
+        this.cleaners.forEach(c => c.clean())
+    }
+}
 
